eb26f7f340c67bb70da8c707498475148dc3517b
SakibSamiul <sb.sakib77@gmail.com>
2024-06-11 00:34:22 +0600

    assignment-01

---
+++ assignment1.py
@@ -0,0 +1,39 @@
+#1. Take values of length and breadth of a from user input() and check if it is square or not.
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+
+if x==y:
+    print("It is Square")
+
+else:
+    print("It is not Square") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
